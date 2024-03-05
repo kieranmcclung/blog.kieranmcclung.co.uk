1ed618c05fca772e004695031478002535aff9b4
Kieran McClung <kieranmcclung@hotmail.com>
2024-03-05 09:51:33 +0000

    💄 Adds sticky positioning to sidebar

--- components/RelatedPosts.tsx
+++ components/RelatedPosts.tsx
@@ -2,12 +2,11 @@ import PostPreview from "./PostPreview";
 
 export default function RelatedPosts({ relatedPosts }: { relatedPosts: any }) {
 	return (
-		<div className="pt-6 lg:pt-10 pb-10">
+		<div className="pt-6 lg:pt-10 lg:relative pb-10">
 			<h3 className="font-heading text-2xl mb-2 tracking-wide">Other stuff</h3>
-			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 mx-auto gap-x-8 gap-y-16">
+			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 lg:opacity-40 lg:sticky lg:top-6 lg:transition-opacity lg:hover:opacity-100 mx-auto gap-x-8 gap-y-16">
 				{relatedPosts.map(
 					(post: any) =>
-						// typeof post !== "undefined" &&
 						typeof post !== "undefined" && (
 							<div key={post.title}>
 								<PostPreview post={post} />
