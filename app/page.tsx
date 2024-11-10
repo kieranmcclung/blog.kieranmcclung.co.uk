ff57a5fc1fa1147a0a7aca0297387949c284ec18
Kieran McClung <kieranmcclung@hotmail.com>
2024-11-10 21:28:34 +0000

    💄 Adds new layout to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { Metadata } from "next";
 import { getAllPosts } from "../lib/api";
 import Pagination from "../components/Pagination";
 import PostGrid from "../components/PostGrid";
+import HeroPostPreview from "../components/HeroPostPreview";
+import FeaturedPreview from "../components/FeaturedPreview";
 
 export const metadata: Metadata = {
 	title: "Kieran McClung",
@@ -19,6 +21,7 @@ export default async function Home() {
 	const posts = await getAllPosts([
 		"title",
 		"date",
+		"heroImage",
 		"featuredImage",
 		"categoryName",
 		"slug",
@@ -28,22 +31,30 @@ export default async function Home() {
 	const totalPosts = posts.length;
 	const postsPerPage = 12;
 	const paginatedPosts =
-		posts.length > postsPerPage - 1 ? posts.slice(0, postsPerPage) : posts;
+		posts.length > postsPerPage - 1 ? posts.slice(3, postsPerPage) : posts;
+	const heroPost = posts.slice(0, 1);
+	const featuredPosts = posts.slice(1, 3);
 
 	return (
-		<div className="container mx-auto p-8 pb-16">
-			<main>
-				<div className="space-y-1 mb-12 md:mt-12">
-					<h1 className="text-5xl md:text-6xl font-heading">Kieran McClung</h1>
-					<p className="text-2xl mb-2 tracking-wide">
-						What&apos;s he up to now?
-					</p>
+		<main>
+			<div className="p-8">
+				<h1 className="font-heading text-2xl">What's he up to?</h1>
+			</div>
+
+			<div className="grid gap-8 mb-8 px-8 lg:grid-cols-[50%_auto] xl:grid-cols-[60%_auto]">
+				{heroPost.map((post: any) => (
+					<HeroPostPreview post={post} key={post.title} />
+				))}
+				<div className="flex flex-col gap-8">
+					{featuredPosts.map((post: any) => (
+						<FeaturedPreview post={post} key={post.title} />
+					))}
 				</div>
+			</div>
 
-				<PostGrid recentPosts={paginatedPosts} />
+			<PostGrid recentPosts={paginatedPosts} />
 
-				<Pagination totalPosts={totalPosts} currentPage={1} />
-			</main>
-		</div>
+			<Pagination totalPosts={totalPosts} currentPage={1} />
+		</main>
 	);
 }
