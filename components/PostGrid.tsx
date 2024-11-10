b823d1b6a245e23c5d8860656f3774300e20e5a5
Kieran McClung <kieranmcclung@hotmail.com>
2024-11-10 21:30:54 +0000

    📱 Tweaks post grid sizes on multiple sized screens

--- components/PostGrid.tsx
+++ components/PostGrid.tsx
@@ -2,7 +2,7 @@ import PostPreview from "./PostPreview";
 
 export default function PostGrid({ recentPosts }: { recentPosts: any }) {
 	return (
-		<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mx-auto gap-x-8 gap-y-16">
+		<div className="grid grid-cols-1 gap-16 px-8 lg:grid-cols-2 3xl:grid-cols-3">
 			{recentPosts.map((post: any) => (
 				<PostPreview post={post} key={post.title} />
 			))}
