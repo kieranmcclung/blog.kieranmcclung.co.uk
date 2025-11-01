246630210cebb5ed82ced30e5d7cc25c67499db6
Kieran McClung <kieran.mcclung@laser.red>
2025-11-01 23:08:32 +0000

    🐛 Fixes issue with sitemap

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -4,21 +4,21 @@ import { getAllPosts } from "../lib/api";
 const EXTERNAL_DATA_URL = "https://blog.kieranmcclung.co.uk";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-	const pages = ["", "art", "bullshit", "design", "gaming", "web"];
-	const pageUrls = pages.map((page) => ({
-		url: `${EXTERNAL_DATA_URL}${page !== "" ? "/" + page : page}`,
-		lastModified: new Date(),
-		priority: page === "" ? 1 : 0.8,
-		changeFrequency: "monthly",
-	}));
+  const pages = ["", "art", "bullshit", "design", "gaming", "web"];
+  const pageUrls = pages.map((page) => ({
+    url: `${EXTERNAL_DATA_URL}${page !== "" ? "/" + page : page}`,
+    lastModified: new Date(),
+    priority: page === "" ? 1 : 0.8,
+    changeFrequency: "monthly",
+  }));
 
-	const posts = await getAllPosts(["date", "categoryName", "slug"]);
-	const postUrls = posts.map((post: any) => ({
-		url: `${EXTERNAL_DATA_URL}/${post.categoryName}/${post.slug}`,
-		lastModified: new Date(post.date),
-		priority: 0.5,
-		changeFrequency: "yearly",
-	}));
+  const posts = await getAllPosts(["date", "categoryName", "slug"]);
+  const postUrls = posts.map((post: any) => ({
+    url: `${EXTERNAL_DATA_URL}/${post.categoryName}/${post.slug}`,
+    lastModified: new Date(post.date),
+    priority: 0.5,
+    changeFrequency: "yearly",
+  }));
 
-	return [...pageUrls, ...postUrls];
+  return [...pageUrls, ...postUrls];
 }
--- lib/api.tsx
+++ lib/api.tsx
@@ -7,133 +7,136 @@ import getReadTime from "./readTime";
 const postsDirectory = join(process.cwd(), "_posts");
 
 const walk: any = async (dirPath: string) =>
-	Promise.all(
-		await readdir(dirPath, { withFileTypes: true }).then((entries) =>
-			entries
-				.filter((entry) => !entry.name.startsWith("."))
-				.map((entry) => {
-					const childPath = join(dirPath, entry.name);
-					return entry.isDirectory() ? walk(childPath) : childPath;
-				})
-		)
-	);
+  Promise.all(
+    await readdir(dirPath, { withFileTypes: true }).then((entries) =>
+      entries
+        .filter((entry) => !entry.name.startsWith("."))
+        .map((entry) => {
+          const childPath = join(dirPath, entry.name);
+          return entry.isDirectory() ? walk(childPath) : childPath;
+        })
+    )
+  );
 
 export async function getPostSlugs(categoryName?: string, slug?: string) {
-	let path = postsDirectory;
+  let path = postsDirectory;
 
-	if (categoryName) {
-		path = postsDirectory + "/" + categoryName;
-	}
+  if (categoryName) {
+    path = postsDirectory + "/" + categoryName;
+  }
 
-	const files = await walk(path);
-	const _files = files.flat(Infinity);
+  const files = await walk(path);
+  const _files = files.flat(Infinity);
 
-	const slugs = _files.map((file: string) => {
-		if (typeof file === "undefined") {
-			return [];
-		}
+  const slugs = _files.map((file: string) => {
+    if (typeof file === "undefined") {
+      return [];
+    }
 
-		const path = file.split("/");
-		const _slug = path[path.length - 1].replace(".md", "");
-		const category = path[path.length - 2];
+    const path = file.split("/");
+    const _slug = path[path.length - 1].replace(".md", "");
+    const category = path[path.length - 2];
 
-		if (typeof slug !== "undefined" && _slug === slug) {
-			return [];
-		}
+    if (typeof slug !== "undefined" && _slug === slug) {
+      return [];
+    }
 
-		if (typeof categoryName !== "undefined" && category !== categoryName) {
-			return [];
-		} else {
-			return {
-				_slug,
-				category,
-			};
-		}
-	}, []);
+    if (typeof categoryName !== "undefined" && category !== categoryName) {
+      return [];
+    } else {
+      return {
+        _slug,
+        category,
+      };
+    }
+  }, []);
 
-	return slugs;
+  return slugs;
 }
 
 export function getPostBySlug(
-	slug: string,
-	categoryName: string,
-	fields: string[] = []
+  slug: string,
+  categoryName: string,
+  fields: string[] = []
 ) {
-	if (typeof slug !== "undefined") {
-		const realSlug = slug.replace(/\.md$/, "");
-		const fullPath = join(
-			postsDirectory + "/" + categoryName,
-			`${realSlug}.md`
-		);
+  if (typeof slug !== "undefined") {
+    const realSlug = slug.replace(/\.md$/, "");
+    const fullPath = join(
+      postsDirectory + "/" + categoryName,
+      `${realSlug}.md`
+    );
 
-		if (fs.existsSync(fullPath)) {
-			const fileContents = fs.readFileSync(fullPath, "utf8");
-			const { data, content } = matter(fileContents);
-			const readTime = getReadTime(content);
+    if (fs.existsSync(fullPath)) {
+      const fileContents = fs.readFileSync(fullPath, "utf8");
+      const { data, content } = matter(fileContents);
+      const readTime = getReadTime(content);
 
-			type Items = {
-				[key: string]: string;
-			};
+      type Items = {
+        [key: string]: string;
+      };
 
-			const items: Items = {};
+      const items: Items = {};
 
-			// Ensure only the minimal needed data is exposed
-			fields.forEach((field) => {
-				if (field === "slug") {
-					items[field] = realSlug;
-				}
-				if (field === "content") {
-					items[field] = content;
-				}
-				if (field === "readTime") {
-					if (typeof data[field] === "undefined") {
-						items[field] = readTime;
-					} else {
-						items[field] = data[field];
-					}
-				}
-				if (field === "categoryName") {
-					items[field] = categoryName;
-				}
+      // Ensure only the minimal needed data is exposed
+      fields.forEach((field) => {
+        if (field === "slug") {
+          items[field] = realSlug;
+        }
+        if (field === "content") {
+          items[field] = content;
+        }
+        if (field === "readTime") {
+          if (typeof data[field] === "undefined") {
+            items[field] = readTime;
+          } else {
+            items[field] = data[field];
+          }
+        }
+        if (field === "categoryName") {
+          items[field] = categoryName;
+        }
 
-				if (typeof data[field] !== "undefined") {
-					items[field] = data[field];
-				}
-			});
+        if (typeof data[field] !== "undefined") {
+          items[field] = data[field];
+        }
+      });
 
-			return items;
-		}
-	}
+      return items;
+    }
+  }
 }
 
 type Slug = {
-	_slug: string;
-	category: string;
+  _slug: string;
+  category: string;
 };
 
 export async function getAllPosts(
-	fields: string[] = [],
-	categoryName?: string
+  fields: string[] = [],
+  categoryName?: string
 ) {
-	const slugs = await getPostSlugs(categoryName);
+  const slugs = await getPostSlugs(categoryName);
 
-	const posts = slugs
-		.map((slug: Slug) => getPostBySlug(slug._slug, slug.category, fields))
-		.sort((post1: any, post2: any) => (post1.date > post2.date ? -1 : 1));
+  const posts = slugs
+    .map((slug: Slug) => getPostBySlug(slug._slug, slug.category, fields))
+    .sort((post1: any, post2: any) => (post1.date > post2.date ? -1 : 1));
 
-	return posts;
+  // remove undefined posts
+  const filteredPosts = posts.filter((post: any) => post !== undefined);
+
+  return filteredPosts;
 }
 
 export async function getRelatedPosts(
-	fields: string[] = [],
-	slug: string,
-	categoryName: string
+  fields: string[] = [],
+  slug: string,
+  categoryName: string
 ) {
-	const slugs = await getPostSlugs(categoryName, slug);
+  const slugs = await getPostSlugs(categoryName, slug);
 
-	const posts = slugs
-		.map((slug: Slug) => getPostBySlug(slug._slug, slug.category, fields))
-		.sort((post1: any, post2: any) => (post1.date > post2.date ? -1 : 1));
+  const posts = slugs
+    .map((slug: Slug) => getPostBySlug(slug._slug, slug.category, fields))
+    .sort((post1: any, post2: any) => (post1.date > post2.date ? -1 : 1));
 
-	return posts;
+  return posts;
 }
